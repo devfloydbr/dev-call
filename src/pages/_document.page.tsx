ec691b3dea3b6e5f5b9810cb138af71583c2fe70
Guilherme J. Floyd <devfloydbr@gmail.com>
2023-01-07 11:23:52 -0300

    :sparkles: feat: Change Home Title end Site Favicon

--- src/pages/_document.page.tsx
+++ src/pages/_document.page.tsx
@@ -16,6 +16,8 @@ export default function Document() {
           rel="stylesheet"
         />
 
+        <link rel="shortcut icon" href="favicon.png" type="img/png" />
+
         <style
           id="stitches"
           dangerouslySetInnerHTML={{ __html: getCssText() }}
